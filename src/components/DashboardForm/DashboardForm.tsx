18cdcc5c03eca93ce1a12fd1cd33788579fc5403
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-06 23:39:58 +0300

    add DashboardForm

--- src/components/DashboardForm/DashboardForm.tsx
+++ src/components/DashboardForm/DashboardForm.tsx
@@ -1,8 +1,64 @@
+import { useDispatch } from 'react-redux';
+import { Formik, ErrorMessage, Field, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
+import { addDashboard } from '../../redux/dashboard-slice';
+import { FormValues } from '../../types';
+import Button from '../Button/Button';
+import css from './DashboardForm.module.scss';
+
+const initialValues = {
+  task: '',
+};
+
+const maxLength = 40 ;
+
+const dashboardSchema = Yup.object().shape({
+  dashboard: Yup.string()
+    .max(maxLength, `Entry length must not exceed ${maxLength} characters`)
+    .required('Enter text'),
+});
+
 const DashboardForm = () => {
-    return (
-        <div>DashboardForm</div>
-    )
-    
-}
+  interface MyFormikHelpers extends FormikHelpers<FormValues> {}
+
+  const dispatch = useDispatch();
+  const handleFormSubmit = (
+    formikValues: FormValues,
+    formikHelpers: MyFormikHelpers
+  ) => {
+    dispatch(addDashboard(formikValues.task));
+    formikHelpers.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={dashboardSchema}
+      onSubmit={handleFormSubmit}
+    >
+      {({ handleSubmit, errors, touched }) => (
+        <>
+          <form className={css.form} onSubmit={handleSubmit}>
+            <Field
+              className={`${css.field} ${
+                touched.task && errors.task ? css.error_field : ''
+              }`}
+              type="text"
+              name="dashboard"
+              placeholder="Enter dashboard title..."
+            />
+
+            <Button type="submit">Add dashboard</Button>
+          </form>
+          <ErrorMessage
+            name="dashboard"
+            component="div"
+            className={css.error}
+          />
+        </>
+      )}
+    </Formik>
+  );
+};
 
 export default DashboardForm;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,5 @@
 import AppBar from '../../components/AppBar/AppBar';
+import DashboardForm from '../../components/DashboardForm/DashboardForm';
 import DashboardList from '../../components/DashboardList/DashboardList';
 import Layout from '../../components/Layout/Layout';
 
@@ -6,6 +7,7 @@ const MainPage = () => {
   return (
     <Layout>
       <AppBar />
+      <DashboardForm />
       <DashboardList />
     </Layout>
   );
