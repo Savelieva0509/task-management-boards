54e745d3673286d0dd0f54da983e3cef5a4edd2c
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-06 23:15:08 +0300

    add  Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,20 @@
-const Modal = () => {
-  return <>Modal</>;
+import { FC, ReactNode } from 'react';
+
+
+import css from './Modal.module.scss';
+
+interface ModalProps {
+    isOpen: boolean;
+    children: ReactNode;
+}
+
+const Modal: FC<ModalProps> = ({ isOpen, children }) => {
+  
+  return (
+      <div className={css.modal}>
+          {children}
+    </div>
+  );
 };
 
-
-export default Modal
+export default Modal;
