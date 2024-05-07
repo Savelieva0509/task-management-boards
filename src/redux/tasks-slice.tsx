163a15ac4ada41b063b053969dacf36729f60cb9
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-08 01:19:50 +0300

    Update tasks-slice.tsx

--- src/redux/tasks-slice.tsx
+++ src/redux/tasks-slice.tsx
@@ -1,66 +1,15 @@
 import { createSlice, PayloadAction, nanoid } from '@reduxjs/toolkit';
-import { TaskTypes, TasksState } from '../types';
+import { TaskStatus } from './constants';
+import { TaskTypes } from '../types';
 
-const initialState: TaskTypes[] = [
-  {
-    id: '0',
-    dashboardId: '1',
-    title: 'Add structure of the project',
-    text: 'Сreate a project structure in accordance with company standards and requirements ',
-    toDo: true,
-    inProcess: true,
-    done: false,
-  },
-  {
-    id: '1',
-    dashboardId: '1',
-    title: 'Implement authentication system',
-    text: 'Implement authentication system using JWT for secure access to the application',
-    toDo: false,
-    inProcess: true,
-    done: false,
-  },
-  {
-    id: '2',
-    dashboardId: '1',
-    title: 'Design user interface',
-    text: 'Design user-friendly interface with modern design principles and responsive layout',
-    toDo: false,
-    inProcess: false,
-    done: true,
-  },
-  {
-    id: '3',
-    dashboardId: '1',
-    title: 'Integrate backend with frontend',
-    text: 'Integrate backend APIs with frontend components to enable data exchange between them',
-    toDo: true,
-    inProcess: false,
-    done: false,
-  },
-  {
-    id: '4',
-    dashboardId: '1',
-    title: 'Write unit tests',
-    text: 'Write comprehensive unit tests to ensure the reliability and stability of the application',
-    toDo: true,
-    inProcess: true,
-    done: false,
-  },
-  {
-    id: '5',
-    dashboardId: '1',
-    title: 'Deploy application to production',
-    text: 'Deploy the application to production server using Docker and Kubernetes for scalability',
-    toDo: false,
-    inProcess: true,
-    done: false,
-  },
+
+export const initialTasksState: TaskTypes[] = [
+  
 ];
 
 const tasksSlice = createSlice({
   name: 'tasks',
-  initialState: initialState,
+  initialState: initialTasksState,
   reducers: {
     addTask: {
       reducer: (state, action: PayloadAction<TaskTypes>) => {
@@ -73,9 +22,7 @@ const tasksSlice = createSlice({
             id: nanoid(),
             title,
             text,
-            toDo: true,
-            inProcess: false,
-            done: false,
+            status: TaskStatus.TODO,
           },
         };
       },
