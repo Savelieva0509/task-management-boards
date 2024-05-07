188554b4afcb2fbaf29cc3bddd84beb400481482
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-07 20:45:10 +0300

    Update Modal.tsx

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,20 +1,13 @@
 import { FC, ReactNode } from 'react';
-
-
 import css from './Modal.module.scss';
 
 interface ModalProps {
-    isOpen: boolean;
-    children: ReactNode;
+  isOpen: boolean;
+  children: ReactNode;
 }
 
 const Modal: FC<ModalProps> = ({ isOpen, children }) => {
-  
-  return (
-      <div className={css.modal}>
-          {children}
-    </div>
-  );
+  return <div className={css.modal}>{children}</div>;
 };
 
 export default Modal;
