9fb19caf36b4954e0c3bdd5d27232c0cc85cd764
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-10 15:28:32 +0300

    update MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,16 +1,35 @@
-import AppBar from '../../components/AppBar/AppBar';
-import DashboardForm from '../../components/DashboardForm/DashboardForm';
-import DashboardList from '../../components/DashboardList/DashboardList';
-import Layout from '../../components/Layout/Layout';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+import { getLastDashboardId,getDashboards } from '../../redux/selectors';
+
+import css from './MainPage.module.scss';
 
 const MainPage = () => {
+  const navigate = useNavigate();
+   const dashboards = useSelector(getDashboards);
+  const lastDashboardId = useSelector(getLastDashboardId);
+
+
+  
   return (
-    <Layout>
-      <AppBar />
-      <DashboardForm />
-      <DashboardList />
-    </Layout>
+    <section className={css.hero}>
+      <div className={css.container}>
+        <h1 className={css.title}>Эффективные решения для вашего бизнеса</h1>
+        {dashboards.length === 0 ? (
+          <Link className={css.link} to={`/dashboards`}>
+            <FaArrowRight style={{ marginRight: '10px' }} />
+            LET START
+          </Link>
+        ) : (
+          <Link className={css.link} to={`/dashboards/${lastDashboardId}`}>
+            <FaArrowRight style={{ marginRight: '10px' }} />
+           LET START
+          </Link>
+        )}
+      </div>
+    </section>
   );
 };
-
 export default MainPage;
